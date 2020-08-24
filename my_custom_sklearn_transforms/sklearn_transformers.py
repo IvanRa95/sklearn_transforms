a7b37ab98473a9200fcbac4d8636f59e133a1703
IvanRa95 <39444649+IvanRa95@users.noreply.github.com>
2020-08-24 16:04:09 -0500

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-
+import pandas as pd
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
@@ -8,9 +8,36 @@ class DropColumns(BaseEstimator, TransformerMixin):
 
     def fit(self, X, y=None):
         return self
-
+    
     def transform(self, X):
-        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        # Primero copiamos el dataframe de datos de entrada 'X'
         data = X.copy()
-        # Retornamos um novo dataframe sem as colunas indesejadas
+        # Devolvemos un nuevo dataframe de datos sin las columnas no deseadas
         return data.drop(labels=self.columns, axis='columns')
+    
+
+class Random_Under_Sampling(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+    def fit(self, X, y=None):
+        return self
+    def transform(self, X):
+        # Primero copiamos el dataframe de datos de entrada 'X'
+        data = X.copy()
+        # separar en las dos clases "aprovado y sospechoso "
+        count_class_0, count_class_1 = data.OBJETIVO.value_counts()
+
+        # nivelar los casos a  527  Df
+        df_class_0 = data[data['OBJETIVO'] == 'Aceptado']
+        df_class_1 = data[data['OBJETIVO'] == 'Sospechoso']
+
+        OBJETIVO_count = data.OBJETIVO.value_counts()
+
+
+        df_class_0_under = df_class_0.sample(count_class_1)
+        df_test_under = pd.concat([df_class_0_under, df_class_1], axis=0)
+
+        print('Random under-sampling:')
+        print(df_test_under.OBJETIVO.value_counts())
+
+        return df_test_under

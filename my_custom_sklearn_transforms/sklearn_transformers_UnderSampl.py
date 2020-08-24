88c1e975887829d16c54a43be0fe8e2d25011202
IvanRa95 <39444649+IvanRa95@users.noreply.github.com>
2020-08-24 17:06:13 -0500

    Update sklearn_transformers_UnderSampl.py

--- my_custom_sklearn_transforms/sklearn_transformers_UnderSampl.py
+++ my_custom_sklearn_transforms/sklearn_transformers_UnderSampl.py
@@ -10,18 +10,7 @@ class RandomUnderSampl(BaseEstimator, TransformerMixin):
     def transform(self, X):
         # Primero copiamos el dataframe de datos de entrada 'X'
         data = X.copy()
+        print(data.columns)
+        
 
-        # nivelar los casos a  527  Df
-        df_class_0 = data[data.iloc[:,-1] == 'Aceptado']
-        df_class_1 = data[data.iloc[:,-1] == 'Sospechoso']
-
-        OBJETIVO_count = data.iloc[:,-1].value_counts()
-
-
-        df_class_0_under = df_class_0.sample(count_class_1)
-        df_test_under = pd.concat([df_class_0_under, df_class_1], axis=0)
-
-        print('Random under-sampling:')
-        print(df_test_under.iloc[:,-1].value_counts())
-
-        return df_test_under
+        return data
